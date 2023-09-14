097a096ca1aa31acb15fac36267d61a966936eb3
aSaxena91 <142254487+aSaxena91@users.noreply.github.com>
2023-09-14 16:18:59 +0530

    deleted old create room

--- Application/FrontEnd/Create_Room/create_room.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    const amenitiesCheckboxes = document.querySelectorAll('input[name="amenities"]:checked');
-    if (amenitiesCheckboxes.length < 2) {
-        alert("Please select at least 2 amenities.");
-        event.preventDefault(); // Prevent form submission if validation fails
-    }
-}
