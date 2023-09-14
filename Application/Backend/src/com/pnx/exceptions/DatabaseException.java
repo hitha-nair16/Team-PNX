ca671bc411a6332d6ed842a3e24ab690851ecf03
seAkk47760 <55938182+seAkk47760@users.noreply.github.com>
2023-09-14 22:35:04 +0530

    Add files via upload

---
+++ Application/Backend/src/com/pnx/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.pnx.exceptions;
+
+public class DatabaseException extends Exception {
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
+
---
+++ Application/Backend/src/com/pnx/exceptions/MeetingNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pnx.exceptions;
+
+public class MeetingNotFoundException extends Exception {
+    public MeetingNotFoundException(String message) {
+        super(message);
+    }
+}
