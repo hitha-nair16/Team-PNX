a4da702563125c29fc8d72e6354c240914d110f9
seAkk47760 <55938182+seAkk47760@users.noreply.github.com>
2023-09-14 15:13:35 +0530

    Add files via upload

--- Application/Backend/db_scripts/user_insertion.sql
+++ Application/Backend/db_scripts/user_insertion.sql
@@ -1,2 +1,16 @@
 insert into users(unique_id,name,email,phone,credits,role)values(11,"Aman","aman@gmail.com","1234567890",100,"Admin");
-select * from users;
+insert into users(unique_id,name,email,phone,credits,role)values(12,"Ayush","ayush@gmail.com","1234567891",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(13,"Aman N","aman1@gmail.com","1234567892",100,"Manager");
+insert into users(unique_id,name,email,phone,credits,role)values(14,"Aditya P","adityap@gmail.com","1234567893",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(15,"Aditya A","adityaa@gmail.com","1234567894",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(16,"Aaseema","aseema@gmail.com","1234567895",100,"Manager");
+insert into users(unique_id,name,email,phone,credits,role)values(17,"Anwesha","anvesha@gmail.com","1234567896",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(18,"Anurag","anurag@gmail.com","1234567897",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(19,"Akash","akash@gmail.com","1234567898",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(20,"Charoo","charoo@gmail.com","1234567810",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(21,"Hitha","hitha@gmail.com","1234567811",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(22,"Pravin","pravin@gmail.com","1234567812",100,"Manager");
+insert into users(unique_id,name,email,phone,credits,role)values(23,"Saumya","saumya@gmail.com","1234567813",100,"User");
+insert into users(unique_id,name,email,phone,credits,role)values(24,"Urvi","urvi@gmail.com","1234567814",100,"Manager");
+insert into users(unique_id,name,email,phone,credits,role)values(25,"Shruti","shruti@gmail.com","1234567815",100,"User");
+
