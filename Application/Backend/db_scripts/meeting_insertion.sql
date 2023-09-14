8c5ccfed7199b2dade520f451ad366ecf390a766
seAkk47760 <55938182+seAkk47760@users.noreply.github.com>
2023-09-14 15:33:00 +0530

    Add files via upload

--- Application/Backend/db_scripts/meeting_insertion.sql
+++ Application/Backend/db_scripts/meeting_insertion.sql
@@ -1,3 +1,5 @@
 insert into meeting(unique_id,title,organised_by,meeting_date,start_time,end_time,meeting_type,members_count,meeting_room_info)values(101,"Room 1",11,'2023-08-23','09:30:00','11:00:00',"Online Training",10,"This Room has been Booked");
-select * from meeting;
-delete from meeting where unique_id=101; 	
+insert into meeting(unique_id,title,organised_by,meeting_date,start_time,end_time,meeting_type,members_count,meeting_room_info)values(102,"Room 2",11,'2023-08-25','09:35:00','11:05:00',"Classroom Training",40,"This Room has been Booked");
+insert into meeting(unique_id,title,organised_by,meeting_date,start_time,end_time,meeting_type,members_count,meeting_room_info)values(103,"Room 3",14,'2023-09-23','10:30:00','12:00:00',"Online Training",20,"This Room has been Booked");
+insert into meeting(unique_id,title,organised_by,meeting_date,start_time,end_time,meeting_type,members_count,meeting_room_info)values(104,"Room 4",14,'2023-09-25','09:30:00','11:00:00',"Conference call",30,"This Room has been Booked");
+insert into meeting(unique_id,title,organised_by,meeting_date,start_time,end_time,meeting_type,members_count,meeting_room_info)values(105,"Room 5",15,'2023-08-26','11:30:00','01:00:00',"Business",40,"This Room has been Booked");
