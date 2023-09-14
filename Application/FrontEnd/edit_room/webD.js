d2d6c52df189205c4bf773515a5d8e58a3276824
aSaxena91 <142254487+aSaxena91@users.noreply.github.com>
2023-09-14 17:02:26 +0530

    uploading create, edit room

---
+++ Application/FrontEnd/edit_room/webD.js
@@ -0,0 +1,66 @@
+// Sample data for rooms (you can replace this with your actual data)
+const roomsData = {
+  room1: {
+      name: "Board Room",
+      seatingCapacity: 20,
+      amenities: ["Projector", "Wi-Fi Connection"]
+  },
+  room2: {
+      name: "Projector Room",
+      seatingCapacity: 8,
+      amenities: ["Wi-Fi Connection", "Whiteboard","TV"]
+  },
+  room3: {
+    name: "Classroom training",
+    seatingCapacity: 40,
+    amenities: ["Wi-Fi Connection", "Coffee machine","TV"]
+}
+  // Add more rooms as needed
+};
+
+const roomSelect = document.getElementById("roomSelect");
+const roomNameInput = document.getElementById("roomName");
+const seatingCapacityInput = document.getElementById("seatingCapacity");
+const amenitiesCheckboxes = document.querySelectorAll('input[name="amenities"]');
+const saveButton = document.querySelector('input[type="submit"]');
+
+// Disable inputs and save button initially
+roomSelect.addEventListener("change", () => {
+  const selectedRoom = roomsData[roomSelect.value];
+  if (selectedRoom) {
+     
+      roomNameInput.value = selectedRoom.name;
+      // document.getElementById("labelName").style.top = "0%";
+ 
+      seatingCapacityInput.value = selectedRoom.seatingCapacity;
+
+      // Check amenities checkboxes based on the selected room's amenities
+      amenitiesCheckboxes.forEach(checkbox => {
+          checkbox.checked = selectedRoom.amenities.includes(checkbox.value);
+      });
+
+      // Enable inputs and save button
+      seatingCapacityInput.disabled = false;
+      amenitiesCheckboxes.forEach(checkbox => {
+          checkbox.disabled = false;
+      });
+      saveButton.disabled = false;
+  } else {
+      // Reset inputs and disable them if no room is selected
+      roomNameInput.value = "";
+      seatingCapacityInput.value = "";
+      amenitiesCheckboxes.forEach(checkbox => {
+          checkbox.checked = false;
+          checkbox.disabled = true;
+      });
+      saveButton.disabled = true;
+  }
+});
+
+function validateForm() {
+  const amenitiesCheckboxesChecked = document.querySelectorAll('input[name="amenities"]:checked');
+  if (amenitiesCheckboxesChecked.length < 2) {
+      alert("Please select at least 2 amenities.");
+      event.preventDefault(); // Prevent form submission if validation fails
+  }
+}
